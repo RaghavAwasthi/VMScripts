d408d54fa5c1f2372f3ccb86e5e61d7bae0d7bf7
Raghav Awasthi <raghavawasthi2014@gmail.com>
2024-05-20 13:25:37 +0530

    Create setup_x11_vnc.sh

---
+++ setup_x11_vnc.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+PASSWORD_SET=$1
+PASSWORD=$2
+
+# Set a password for x11vnc if provided
+if [ "$PASSWORD_SET" = true ]; then
+    mkdir -p ~/.vnc
+    echo "$PASSWORD" | x11vnc -storepasswd stdin ~/.vnc/passwd
+    X11VNC_OPTIONS="-rfbauth /home/$USER/.vnc/passwd"
+else
+    X11VNC_OPTIONS=""
+fi
+
+# Create x11vnc service
+cat << EOF | sudo tee /etc/systemd/system/x11vnc.service
+[Unit]
+Description=Start x11vnc at startup.
+After=multi-user.target
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/x11vnc -display :0 -forever -shared -auth guess $X11VNC_OPTIONS
+Restart=always
+RestartSec=10
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Enable and start x11vnc service
+sudo systemctl enable x11vnc.service
+sudo systemctl start x11vnc.service
