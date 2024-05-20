ad397322be18b512170186fae7dcdf9397c6d1a1
Raghav Awasthi <raghavawasthi2014@gmail.com>
2024-05-20 14:04:37 +0530

    Update setup_base_package.sh

--- setup_base_package.sh
+++ setup_base_package.sh
@@ -3,4 +3,3 @@
 # Update package list and install necessary packages
 sudo apt update
 sudo apt install -y lxde-core lxterminal xorg dbus-x11 x11-xserver-utils
-sudo apt install -y x11vnc nginx git openssl
