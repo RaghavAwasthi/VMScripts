cd48aca54ae10ad230475b4726a3bfe25115bc73
Raghav Awasthi <raghavawasthi2014@gmail.com>
2024-07-08 00:36:16 +0530

    Update setup_nginx.sh

--- setup_nginx.sh
+++ setup_nginx.sh
@@ -1,42 +1,50 @@
 #!/bin/bash
 sudo apt install -y nginx git openssl
 # Generate a self-signed SSL certificate
-# sudo mkdir -p /etc/nginx/ssl
-# sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/selfsigned.key -out /etc/nginx/ssl/selfsigned.crt -subj "/CN=localhost"
-# sudo openssl dhparam -out /etc/nginx/ssl/dhparam.pem 2048
+sudo mkdir -p /etc/nginx/ssl
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/selfsigned.key -out /etc/nginx/ssl/selfsigned.crt -subj "/CN=localhost"
+sudo openssl dhparam -out /etc/nginx/ssl/dhparam.pem 2048
 
 # Set permissions for SSL files
-# sudo chown root:www-data /etc/nginx/ssl/selfsigned.key /etc/nginx/ssl/selfsigned.crt /etc/nginx/ssl/dhparam.pem
-# sudo chmod 640 /etc/nginx/ssl/selfsigned.key
-# sudo chmod 644 /etc/nginx/ssl/selfsigned.crt
-# sudo chmod 644 /etc/nginx/ssl/dhparam.pem
+sudo chown root:www-data /etc/nginx/ssl/selfsigned.key /etc/nginx/ssl/selfsigned.crt /etc/nginx/ssl/dhparam.pem
+sudo chmod 640 /etc/nginx/ssl/selfsigned.key
+sudo chmod 644 /etc/nginx/ssl/selfsigned.crt
+sudo chmod 644 /etc/nginx/ssl/dhparam.pem
 
-# # Create SSL configuration snippets for nginx
-# sudo tee /etc/nginx/snippets/self-signed.conf > /dev/null <<EOF
-# ssl_certificate /etc/nginx/ssl/selfsigned.crt;
-# ssl_certificate_key /etc/nginx/ssl/selfsigned.key;
-# EOF
+# Create SSL configuration snippets for nginx
+sudo tee /etc/nginx/snippets/self-signed.conf > /dev/null <<EOF
+ssl_certificate /etc/nginx/ssl/selfsigned.crt;
+ssl_certificate_key /etc/nginx/ssl/selfsigned.key;
+EOF
 
-# sudo tee /etc/nginx/snippets/ssl-params.conf > /dev/null <<EOF
-# ssl_protocols TLSv1.2;
-# ssl_prefer_server_ciphers on;
-# ssl_ciphers "ECDHE-RSA-AES128-GCM-SHA256:HIGH:!aNULL:!MD5:!3DES";
-# ssl_dhparam /etc/nginx/ssl/dhparam.pem;
-# ssl_session_timeout 1d;
-# ssl_session_cache shared:SSL:50m;
-# ssl_stapling on;
-# ssl_stapling_verify on;
-# add_header Strict-Transport-Security "max-age=15768000" always;
-# EOF
+sudo tee /etc/nginx/snippets/ssl-params.conf > /dev/null <<EOF
+ssl_protocols TLSv1.2;
+ssl_prefer_server_ciphers on;
+ssl_ciphers "ECDHE-RSA-AES128-GCM-SHA256:HIGH:!aNULL:!MD5:!3DES";
+ssl_dhparam /etc/nginx/ssl/dhparam.pem;
+ssl_session_timeout 1d;
+ssl_session_cache shared:SSL:50m;
+ssl_stapling on;
+ssl_stapling_verify on;
+add_header Strict-Transport-Security "max-age=15768000" always;
+EOF
 
 # Configure nginx
 sudo tee /etc/nginx/sites-available/default > /dev/null <<EOF
-
 server {
-    listen 80 ;
+    listen 80;
     server_name _;
 
-    
+    # Redirect all HTTP requests to HTTPS
+    return 301 https://\$host\$request_uri;
+}
+
+server {
+    listen 443 ssl;
+    server_name _;
+
+    include snippets/self-signed.conf;
+    include snippets/ssl-params.conf;
 
     location / {
         root /opt/noVNC;
