e093fd9a97d05d4162666c350a8b3cdf6d34750b
Raghav Awasthi <raghavawasthi2014@gmail.com>
2024-07-08 00:40:16 +0530

    Update setup_tight.sh

--- setup_tight.sh
+++ setup_tight.sh
@@ -21,27 +21,3 @@ EOF
 sudo systemctl daemon-reload
 sudo systemctl start tightvnc
 sudo systemctl enable tightvnc
-root@ip-172-31-44-32:~/VMScripts# cat setup_tight.sh 
-#!/bin/bash
-
-# Create tiger service
-cat << EOF | sudo tee /etc/systemd/system/tightvnc.service
-[Unit]
-Description=Start TightVNC server at startup
-After=syslog.target network.target
-
-[Service]
-Type=forking
-User=ubuntu
-ExecStartPre=-/usr/bin/vncserver -kill :1 
-ExecStart=/usr/bin/vncserver :1
-ExecStop=/usr/bin/vncserver -kill :1
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-# Reload systemd, start and enable VNC service
-sudo systemctl daemon-reload
-sudo systemctl start tightvnc
-sudo systemctl enable tightvnc
